dde16bc12348e52f8e40d38a50c6a4bb76605670
shanzuiyixing <3274911246@qq.com>
2024-11-01 16:05:56 +0800

    完善图

--- docs/Test.cpp
+++ docs/Test.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
+#include <bitset>
+#include <cmath>
 using namespace std;
 int main() {
-    int A[10] = {0};
-    cout << sizeof(A)/sizeof(A[0]);
-    return 0;
+    unsigned int a = 134;
+    unsigned int b = 246;
+    unsigned int c = a - b;
+    unsigned int d = 4294967184;
+    int e = d;
+    int g = 65536;
+    unsigned short int f =  65535;
+    short int h = 65535;
+    float i = -1;
+    cout << h << " " << bitset<sizeof(h)*8>(h) << endl;
+    cout << bitset<sizeof(g)*8>(g) << endl;
+    cout << bitset<sizeof(f)*8>(f) << endl << f <<endl << g << endl;
+    cout << d << endl << e << endl;
+    cout << c << endl << bitset<sizeof(a)*8>(a);
+    cout << endl << bitset<sizeof(b)*8>(b);
+    cout << endl << bitset<sizeof(-b)*8>(-b);
+    cout << endl << bitset<sizeof(c)*8>(c);
+
 }
